9f552dca80b5392fdff5dc68311d2f1d770e5952
Armin Weihbold <koyaan@users.noreply.github.com>
2013-10-09 15:54:31 +0200

    Update snippets.sh

--- snippets.sh
+++ snippets.sh
@@ -3,3 +3,6 @@ find . -name ".svn" -exec rm -rf {} \;
 
 #show active dhcp leases
 cat /var/lib/dhcp/dhcpd.leases
+
+#download multiple files with curl
+curl -O "https://az412801.vo.msecnd.net/vhd/IEKitV1_Final/VirtualBox/OSX/IE10_Win8/IE10.Win8.For.MacVirtualBox.part{1.sfx,2.rar,3.rar}"
