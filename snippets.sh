c1c0d82b8916a59e61e182d49c692d2bdc86a90d
Armin Weihbold <koyaan@users.noreply.github.com>
2013-11-20 10:31:00 +0100

    add symfony permissions script

--- snippets.sh
+++ snippets.sh
@@ -9,3 +9,8 @@ curl -O "https://az412801.vo.msecnd.net/vhd/IEKitV1_Final/VirtualBox/OSX/IE10_Wi
 
 #batch resize images with imagemagick
 $ for i in $( ls *.jpg); do convert -resize 50% $i re_$i; done
+
+#set symfony permissions
+APACHEUSER=`ps aux | grep -E '[a]pache|[h]ttpd' | grep -v root | head -1 | cut -d\  -f1`
+sudo setfacl -R -m u:$APACHEUSER:rwX -m u:`whoami`:rwX app/cache app/logs
+sudo setfacl -dR -m u:$APACHEUSER:rwX -m u:`whoami`:rwX app/cache app/logs
